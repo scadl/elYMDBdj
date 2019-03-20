b6a6d0a5e3ce0e7c379f78dbb84ad3c96b6b5074
Alexandr KD <scad.luncher@gmail.com>
2019-03-20 18:39:46 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,133 @@
+ //handle setupevents as quickly as possible
+ const setupEvents = require('./setupEvent')
+ if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+ } else {
+
+     /* main Electron working code */
+     const {app, BrowserWindow, Menu, Tray, nativeImage} = require('electron');
+     let win;
+
+     function createWindow() {
+
+         win = new BrowserWindow({
+             width: 1280,
+             height: 800,
+             minWidth: 320,
+             minHeight: 240,
+             acceptFirstMouse: true,
+             titleBarStyle: "hidden",
+             autoHideMenuBar: true,
+             frame: true,
+             backgroundColor: '#343a40',
+             icon: 'res/site_icon_x32.png'
+         });
+
+         /*
+         new BrowserWindow({
+             transparent:true
+             frame: false,
+             backgroundColor: '#00FFFFFF'
+         });*/
+
+         //win.webContents.openDevTools();
+
+         win.loadURL('file://' + __dirname + '/src/myLoaderView.html');
+         win.show();
+
+        // This twisted CWD argument is required to correctly drive django's Model.ImageFile path
+         var djangoSpawn = require('child_process').spawn;
+         var djangoWin = djangoSpawn(__dirname+'/djYMDB/venv/Scripts/python', ['-u', __dirname+'/djYMDB/manage.py', 'runserver'], {cwd:__dirname+'/djYMDB'});
+         djangoWin.stdout.on('data', function (data) {
+             console.log(data.toString());
+         });
+
+         let myTray = null;
+         const nImage = nativeImage.createFromPath(__dirname+'/res/site_icon_x32.png');
+         const trayMeu = Menu.buildFromTemplate([
+             {label:'Добавить запись', type:"normal", click:function () {
+                     win.restore();
+                     win.loadURL('http://localhost:8000/add/0');
+                     win.show();
+                 }},
+             {label:'Изменить колллекции', type:"normal", click:function () {
+                     win.restore();
+                     win.loadURL('http://localhost:8000/prop/1');
+                     win.show();
+                 }},
+             {label:'Изменить жанры', type:"normal", click:function () {
+                     win.restore();
+                     win.loadURL('http://localhost:8000/prop/2');
+                     win.show();
+                 }},
+             {label:'sep', type:"separator"},
+             {label:'Покинуть программу', type:"normal", click:function () {
+                     app.exit(0);
+                 }}
+         ]);
+
+         var procsPIDS = new Array();
+         var redirs = 0;
+         win.webContents.on('did-finish-load', function () {
+
+             redirs += 1;
+             if (redirs == 2) {
+
+                 // We have loaded main server.
+                 // Now can collect python PIDs
+                 const tasklist = require('tasklist');
+                 tasklist({verbose: true, filter: ['Imagename eq python.exe']}).then(tasks => {
+                     //console.log(tasks);
+                     tasks.forEach(function (t) {
+                         procsPIDS.push(t.pid);
+                     });
+                     console.log(procsPIDS);
+                 });
+
+                 // Also tray icon
+                 myTray = new Tray(nImage);
+                 myTray.setContextMenu(trayMeu);
+                 myTray.setToolTip('YMDB - Ваша База Художественных Произведений');
+                 myTray.on('click', function () {
+                     win.restore();
+                     win.show();
+                     win.focus();
+                 });
+
+             }
+
+         });
+
+         win.on('closed', function() {
+
+             procsPIDS.forEach(function (pypid) {
+                 process.kill(pypid);
+             })
+
+         });
+
+         win.on('minimize', function() {
+             win.hide();
+             if (myTray){
+                 myTray.displayBalloon({icon:'res/site_icon_x32.png',title:'YMDB',
+                     content:'Программа продолжает работать.\nЩелкните чтобы отбразить окно.\nИли воспользутесь меню'})
+             }
+         })
+
+
+     }
+
+     app.on('ready', createWindow);
+     app.on('window-all-closed', () => {
+         if (process.platform !== 'darwin') {
+             app.quit();
+         }
+
+     });
+     app.on('activate', () => {
+         if (win === null) {
+             createWindow();
+         }
+     });
+ }
---
+++ src/myLoader.js
@@ -0,0 +1,67 @@
+'use strict'
+
+//window.$ = window.jQuery = require('jquery');
+window.Bootstrap = require('bootstrap');
+
+(function() {
+
+    const remote = require('electron').remote;
+
+    function serverExpose(){
+        let win = remote.getCurrentWindow();
+        win.loadURL('http://localhost:8000');
+    }
+
+    function PyInstall(){
+        document.querySelector('#appStatus').innerHTML = '<span class="text-warning">Настройка Python 3.7</span>'
+        document.querySelector("#pynote").innerHTML = 'N/A';
+        document.querySelector("#djnote").innerHTML = 'N/A'
+
+        var pyInstSpawn = require('child_process').spawn;
+        var pyInst = pyInstSpawn(__dirname+'/dist/python-3.7.1-amd64.exe', ['/uninstall', '/quiet']);
+        pyInst.on('exit', function (dataP) {
+            var pyInst2 = pyInstSpawn(__dirname+'/dist/python-3.7.1-amd64.exe', ['/quiet', 'PrependPath=1']);
+            pyInst2.on('exit', function (dataP2) {
+                remote.dialog.showMessageBox({type:'warning', icon: remote.nativeImage.createFromPath(__dirname+'/res/Python-logo.png'),
+                    title:'Внимание', message:'Были внесены изменения в систему.\nПерезапустите программу.'});
+                remote.app.exit(0);
+            });
+        });
+    }
+
+    document.onreadystatechange = function() {
+        if (document.readyState == "complete") {
+
+            document.querySelector('#appStatus').innerHTML = '<span class="text-white-50">Загрузка...</span>';
+
+            var pyVer = 'N/A';
+            const { exec } = require('child_process');
+            exec('python -V', (err, stdout, stderr)=>{
+                if (err){
+                    PyInstall();
+                    return;
+                }
+
+                var pyVer = '';
+                pyVer = stdout.split(' ');
+                if( pyVer[1].indexOf('3.') != -1 ) {
+
+                    document.querySelector("#pynote").innerHTML = pyVer[1];
+
+                    var djVSpawn = require('child_process').spawn;
+                    var djV = djVSpawn(__dirname+'/../djYMDB/venv/Scripts/python', [__dirname+'/../djYMDB/manage.py', 'version']);
+                    djV.stdout.on('data', function (dataD) {
+                        document.querySelector("#djnote").innerHTML = dataD.toString();
+                        setTimeout(serverExpose(), 5);
+                    });
+
+                } else{
+                    PyInstall();
+                }
+
+            });
+
+        }
+    };
+
+})();
